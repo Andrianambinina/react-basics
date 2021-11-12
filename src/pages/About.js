53562c2e0a609269845bbe5e8d99963b0bb5c649
Zo Andrianambinina <zo@livenexx.net>
2021-11-12 14:24:44 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <Logo />
+            <h1>À propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dolorem nihil labore assumenda enim nobis reprehenderit iusto soluta vitae! Perferendis veniam eligendi sapiente obcaecati earum. Deserunt repudiandae labore quidem quasi amet cumque omnis expedita quo aliquid nam ut sapiente, cupiditate, adipisci aut accusamus porro a recusandae aspernatur magni nisi totam. Provident adipisci, ad cupiditate repellendus beatae repellat necessitatibus harum recusandae?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto natus optio aut quidem aliquam, sequi ipsum debitis architecto illum voluptatum. Doloribus voluptatum ullam accusantium corrupti, dolor deleniti, aspernatur neque officia necessitatibus, aperiam quos voluptatibus? Aspernatur accusantium sint sunt saepe nulla dolorum voluptas adipisci rerum iste dignissimos, impedit quidem, ex atque ea quas modi architecto reprehenderit corrupti culpa magni recusandae molestiae natus esse quis! Laudantium vel consequuntur perspiciatis, eaque praesentium tempora sed sapiente, expedita quasi, debitis alias eligendi totam vitae eveniet libero ab exercitationem unde nesciunt asperiores omnis incidunt. Id iure natus voluptatum modi! Incidunt assumenda soluta laborum eaque, magnam nobis temporibus odio ipsa commodi fugiat, quae dolorum vero tempore officiis! Obcaecati molestiae sequi sint aliquid iure voluptas temporibus numquam hic inventore quos vero aperiam corrupti quo iste, commodi id est odio ratione necessitatibus ducimus? Iste odit adipisci placeat illo impedit.</p>
+        </div>
+    );
+};
+
+export default About;
